de255f5ea1d9660704df7ae5582cbd1f4dd56699
Philip Lewis <pcl@pclewis.com>
2014-03-10 22:46:09 -0400

    Change license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject tris "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "CC0"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [hiccup "1.0.5"]
